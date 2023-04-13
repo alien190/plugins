1e4fdb26098777e224173ae73cfcb590454d12e3
nikolay <mylevin40@gmail.com>
2023-04-13 15:33:09 +0700

    iOS: Implements reinit barcode hints for the each frame.

--- packages/camera/camera/ios/Classes/CameraPlugin.m
+++ packages/camera/camera/ios/Classes/CameraPlugin.m
@@ -461,7 +461,6 @@ AVCaptureAudioDataOutputSampleBufferDelegate>
 @property(assign, atomic) int barcodeCropTop;
 @property(assign, atomic) int barcodeCropBottom;
 @property(readonly, nonatomic) ZXMultiFormatReader *barcodeReader;
-@property(readonly, nonatomic) ZXDecodeHints *barcodeHints;
 @property(nonatomic) FlutterEventChannel *barcodeEventChannel;
 @property(nonatomic) BarcodeStreamHandler *barcodeStreamHandler;
 @end
@@ -718,12 +717,6 @@ NSString *const errorMethod = @"error";
 
 - (void)start {
     if(_isBarcodeStreamEnabled) {
-        _barcodeHints = [ZXDecodeHints hints];
-        [_barcodeHints addPossibleFormat:kBarcodeFormatEan8];
-        [_barcodeHints addPossibleFormat:kBarcodeFormatEan13];
-        [_barcodeHints addPossibleFormat:kBarcodeFormatRSSExpanded];
-        [_barcodeHints addPossibleFormat:kBarcodeFormatRSS14];
-        
         _barcodeReader = [ZXMultiFormatReader reader];
         _isNextFrameToBarcodeScanRequired = true;
         
@@ -1195,14 +1188,20 @@ didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
 
 - (void)recognizeBarcode:(ZXLuminanceSource*)source {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        if(self->_barcodeReader != nil && self->_barcodeHints != nil && self->_barcodeStreamHandler != nil && self->_barcodeStreamHandler.eventSink != nil) {
+        if(self->_barcodeReader != nil && self->_barcodeStreamHandler != nil && self->_barcodeStreamHandler.eventSink != nil) {
             
             ZXBinaryBitmap *bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
             
             NSError *error = nil;
             
+            ZXDecodeHints* barcodeHints = [ZXDecodeHints hints];
+            [barcodeHints addPossibleFormat:kBarcodeFormatEan8];
+            [barcodeHints addPossibleFormat:kBarcodeFormatEan13];
+            [barcodeHints addPossibleFormat:kBarcodeFormatRSSExpanded];
+            [barcodeHints addPossibleFormat:kBarcodeFormatRSS14];
+            
             ZXResult *result = [self->_barcodeReader decode:bitmap
-                                                      hints:self->_barcodeHints
+                                                      hints:barcodeHints
                                                       error:&error];
             if (result) {
                 NSMutableDictionary* dict = [NSMutableDictionary dictionary];
